7b3df4f796a75408d031df08e1168c912db76a50
Brian Dunn <brian@theophil.us>
2015-06-24 16:53:35 -0400

    Break up components

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -14,48 +14,77 @@ let get = function (url, query) {
   });
 };
 
-let App = React.createClass({
+let Form = React.createClass({
   getInitialState() {
-    return {events: [], term: '', requests: 0};
+    return {term: '', requests: 0};
   },
   search(e) {
     e.preventDefault();
     let term = e.target.search.value;
     this.setState({term: term, requests: this.state.requests + 1});
     get(`${API_URL}/events.json?q=${term}`).then(function (response) {
+      if (response.events !== this.state.events) {
+        this.props.onResultsChange(response.events, term);
+      }
       this.setState({events: response.events, requests: this.state.requests - 1});
     }.bind(this));
-  },
-  chart() {
-    let chart = '';
-    if (this.state.events.length && this.state.requests === 0) {
-      chart = (
-          <Chart data={this.state.events} term={this.state.term}/>
-      );
-    }
-    return chart;
   },
   summary() {
     let summary;
     if (this.state.requests > 0) {
       summary = (<p>Searching for "{this.state.term}" &hellip;</p>);
     } else if (this.state.term) {
-      summary = (<p>{this.state.events.length} events matched "{this.state.term}"</p>);
+      summary = (
+        <section>
+        <p>{this.state.events.length} events matched "{this.state.term}"</p>
+        </section>
+      );
     }
     return summary;
   },
   render() {
     return (
-      <main>
       <form onSubmit={this.search}>
         <label htmlFor='search'>Search</label>
         <input id='search' type='search' autoFocus />
         <input type='submit' value='Search' />
+        {this.summary()}
       </form>
+    );
+  }
+});
+
+let Results = React.createClass({
+  getInitialState() {
+    return {results: []};
+  },
+  chart() {
+    let chart = '';
+    if (this.state.results.length) {
+      chart = (
+          <Chart data={this.state.results} term={this.state.term}/>
+      );
+    }
+    return chart;
+  },
+  render() {
+    return (
       <section id='results'>
-      {this.summary()}
-      {this.chart()}
+        {this.chart()}
       </section>
+    );
+  }
+});
+
+let App = React.createClass({
+  resultsChanged(results, term) {
+    this.refs.results.setState({results: results, term: term});
+  },
+  render() {
+    return (
+      <main>
+        <Form onResultsChange={this.resultsChanged} />
+        <Results ref='results' />
       </main>
     );
   }
