3b1d1daac26591f8a86beaab4e9c161cd1f2a142
Brian Dunn and Dillon Hafer <dev+briandunn+dillonhafer@hashrocket.com>
2015-07-01 16:49:18 -0500

    Add series to totals for selected weight range

--- frontend/src/weight-chart.jsx
+++ frontend/src/weight-chart.jsx
@@ -5,6 +5,14 @@ import Highcharts from 'react-highcharts';
 let WeightChart = React.createClass({
   propTypes: {
     data: React.PropTypes.array.isRequired,
+    select: React.PropTypes.func
+  },
+  getDefaultProps() {
+    return {
+      select: function (min, max) {
+        console.log(min, max);
+      }
+    };
   },
   shouldComponentUpdate(props, state) {
     return !_.isEqual(this.props.data, props.data);
@@ -20,6 +28,24 @@ let WeightChart = React.createClass({
     return {
       title: {text: 'Adverse Events by Weight'},
       credits: { text: 'open fda', href: 'https://open.fda.gov' },
+      chart: {
+        zoomType: 'x',
+        events: {
+          selection: (e)=> {
+            e.preventDefault();
+            let {min: min, max: max, axis: axis} = e.xAxis[0];
+
+            axis.removePlotBand('selection-band');
+            axis.addPlotBand({
+              id: 'selection-band',
+              from: min,
+              to: max,
+              color: 'rgba(0, 0, 0, 0.2)'
+            });
+            this.props.select(min, max);
+          }
+        }
+      },
       xAxis: {
         title: {text: 'Weight (kg)'}
       },
